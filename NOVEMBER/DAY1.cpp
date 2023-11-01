078a6da1750574f21524caf0a568d88aaf975b10
DHRUV KHANDELWAL <khandelwaldhruv2003@gmail.com>
2023-11-01 21:27:13 +0530

    firstday

---
+++ NOVEMBER/DAY1.cpp
@@ -0,0 +1,71 @@
+//Frequencies of Limited Range Array Elements
+//Easy //Accuracy: 27.64
+// Given an array arr[] of N positive integers which can contain integers from 1 to P where elements can be repeated or can be absent from the array. Your task is to count the frequency of all numbers from 1 to N. Make in-place changes in arr[], such that arr[i] = frequency(i). Assume 1-based indexing.
+// Note: The elements greater than N in the array can be ignored for counting and do modify the array in-place. 
+
+
+//Code
+
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std; 
+
+// } Driver Code Ends
+class Solution{
+    public:
+    //Function to count the frequency of all elements from 1 to N in the array.
+    void frequencyCount(vector<int>& arr,int N, int P)
+    { 
+        // code here
+        unordered_map<int,int> mp;
+        for(int i=0;i<N;i++){
+            mp[arr[i]]++;
+        }
+        for(int i=1;i<=N;i++){
+            arr[i-1]=mp[i];
+        }
+    }
+};
+
+        
+        
+
+//{ Driver Code Starts.
+
+int main() 
+{ 
+	long long t;
+	
+	//testcases
+	cin >> t;
+	
+	while(t--){
+	    
+	    int N, P;
+	    //size of array
+	    cin >> N; 
+	    
+	    vector<int> arr(N);
+	    
+	    //adding elements to the vector
+	    for(int i = 0; i < N ; i++){
+	        cin >> arr[i]; 
+	    }
+        cin >> P;
+        Solution ob;
+        //calling frequncycount() function
+		ob.frequencyCount(arr, N, P); 
+		
+		//printing array elements
+	    for (int i = 0; i < N ; i++) 
+			cout << arr[i] << " ";
+	    cout << endl;
+	}	
+	return 0; 
+}
+
+
+
+
+
+// } Driver Code Ends
